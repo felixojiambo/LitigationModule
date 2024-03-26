19ef904d6411c254c53294205d35ee7439ffb88a
Felix Ojiambo <felixojiamboe@gmail.com>
2024-03-26 00:26:41 -0700

    defining dto for ligation

---
+++ src/main/java/com/emtech/Litigation/dtos/LitigationCase.java
@@ -0,0 +1,46 @@
+package com.emtech.Litigation.dtos;
+import com.emtech.Litigation.enums.CaseStatus;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class LitigationCase {
+        private Long id;
+        private String caseReferenceNumber;
+        private String firstName;
+        private String lastName;
+        private String middleName;
+        private String clientEmail;
+        private String clientPhoneNumber;
+        private String loanId;
+        private Double loanAmount;
+        private String loanType;
+        private String collateralDetails;
+        private String insuranceCoverage;
+        private String securityAttached;
+        private String documentList;
+        private Long cifId;
+        private Date loanStartDate;
+        private Date loanEndDate;
+        private Long idNumber;
+        private String kraPinNumber;
+        private Long alternativePhoneNumber;
+        private String postalAddress;
+        private String postalCode;
+        private String city;
+        private String country;
+        private String clientCode;
+        private Long loanAccountNumber;
+        private String interestRate;
+        private Date defaultDate;
+        private Double amountRemaining;
+        private String loanDescription;
+        private Date caseCreationDate;
+        private String caseCreatedBy;
+        private String caseNotes;
+        private CaseStatus caseStatus;
+    }
+
