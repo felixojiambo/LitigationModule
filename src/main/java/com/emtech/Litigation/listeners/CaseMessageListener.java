d8c9f712645789cca6468d6e1284d280d2f40152
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-07 23:52:28 -0700

    case message queue listener from client management

---
+++ src/main/java/com/emtech/Litigation/listeners/CaseMessageListener.java
@@ -0,0 +1,22 @@
+package com.emtech.Litigation.listeners;
+import com.emtech.Litigation.dtos.LitigationCaseDTO;
+import com.emtech.Litigation.services.LitigationCaseInitiator;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CaseMessageListener {
+
+    private final LitigationCaseInitiator litigationCaseInitiator;
+
+    @Autowired
+    public CaseMessageListener(LitigationCaseInitiator litigationCaseInitiator) {
+        this.litigationCaseInitiator = litigationCaseInitiator;
+    }
+
+    @RabbitListener(queues = "newCaseQueue")
+    public void handleNewCase(LitigationCaseDTO litigationCaseDTO) {
+        litigationCaseInitiator.processCaseData(litigationCaseDTO);
+    }
+}
